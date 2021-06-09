798e0fe219392e31a342ae0e567a1b1ffe404a23
Joep Barmentlo <jbarment@student.42.fr>
2021-06-09 19:40:52 +0200

    can launch a sim

--- client/client.py
+++ client/client.py
@@ -2,11 +2,11 @@
 import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 8081        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(int.to_bytes(9099, byteorder='little', length=1024))
     data = s.recv(1024)
 
 print('Received', repr(data))
