411690761322da5d351cf4273178f56f89420110
Joep Barmentlo <jbarment@student.42.fr>
2021-06-10 16:27:16 +0200

    yoooooo

--- src/config/net_config.py
+++ src/config/net_config.py
@@ -8,6 +8,7 @@ net_config.server_port = 9089
 net_config.server_refresh_time = 1 # seconds to wait before listening again
 
 net_config.start_sim_request = "start sim"
+net_config.stop_sim_request = "stop sim"
 net_config.ping_request = "ping"
 
 # * The Host option remains unused for now
